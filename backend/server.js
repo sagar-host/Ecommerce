4b5a7faec1bf94f84aacaef0597773d5ba59006b
sagar <rebelsagarx@gmail.com>
2022-03-02 18:17:32 +0530

    Connect Node With MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
+
 
 //for config
 
 dotenv.config({path: "backend/config/config.env"})
+
+//connect to database
+connectDatabase()
 
 
 
