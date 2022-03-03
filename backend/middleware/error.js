cac282729c4dd8061c91a41f69fb5b2d8249057d
sagar <rebelsagarx@gmail.com>
2022-03-03 22:53:09 +0530

    Backend Authentication

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -5,9 +5,30 @@ module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
-    //wrong mongodb error
+    //wrong mongodb  id error
     if(err.name === 'CastError'){
         const message = `Resources not found. Invalid: ${err.path}`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    //mongoose duplicate key error
+    if(err.code === 11000){
+        const message =`Duplicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message, 400);
+
+    }
+
+       //wrong JWT  error
+       if(err.name === 'jsonWebTokenError'){
+        const message = `Json web Token is invalid, try again`;
+        err = new ErrorHandler(message, 400);
+    }
+
+
+    
+       //JWT Expire error
+       if(err.name === 'TokenExpiredError'){
+        const message = `Json web Token is Expired, try again`;
         err = new ErrorHandler(message, 400);
     }
 
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
+const productSchema =  new mongoose.Schema({
     name:{
         type:String,
         required:[true,"Please Enter product Name"],
@@ -59,6 +59,12 @@ const productSchema = mongoose.Schema({
             }
         }
     ],
+
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:"User",
+        required:true,
+    },
     createdAt:{
         type:Date,
         default:Date.now
